1566a23b81f625c715ec361244005982ed500029
Michael Luo <michael.luo@berkeley.edu>
2023-03-06 04:33:18 +0000

    temp

--- skyburst/job.py
+++ skyburst/job.py
@@ -9,9 +9,16 @@ class Job(object):
         self.num_gpus = resources['GPUs']
         self.cost = cost
 
+        # State of the Job
         self.state = None
+        # Starting time of the job on the local cluster, if none, the job was ran on the cloud.
         self.start = None
+
+        # Keeps track of which GPU(s) the job ran on.
         self.allocated_gpus = {}
+
+        # This for preemption, to prevent chain pre-emptions
+        self.opp_cost = 0
 
     def __eq__(self, other):
         return self.idx == other.idx
