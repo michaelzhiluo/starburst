5e2913cb5bc94c01ad4c1ff118ebd4b183bb42b0
Siyuan <suquark@gmail.com>
2023-05-09 00:30:59 -0700

    update

--- cluster_setup/gcp/get_logs.sh
+++ cluster_setup/gcp/get_logs.sh
@@ -2,4 +2,7 @@
 job_name=$1
 kubectl get -o yaml jobs "$1"
 echo "======================"
-kubectl logs $(kubectl get pods --selector=job-name="$1" -o jsonpath='{.items[*].metadata.name}')
+kubectl get pods --selector=job-name="$1"
+echo "======================"
+# kubectl logs $(kubectl get pods --selector=job-name="$1" -o jsonpath='{.items[*].metadata.name}')
+kubectl logs $(kubectl get pods --selector=job-name="$1" -o jsonpath='{.items[-1].metadata.name}')
