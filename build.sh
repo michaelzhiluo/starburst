cab8324170e4cae64683a039d3e5721915488260
Michael Luo <michael.luo@berkeley.edu>
2024-05-12 02:28:59 +0000

    Clean docker file

--- build.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-# Build the docker image and push to GCR
-docker build -t skyburst:latest .
-docker tag skyburst:latest gcr.io/sky-burst/skyburst:latest
-docker push gcr.io/sky-burst/skyburst:latest
--- dockerfile
+++
@@ -1,3 +0,0 @@
-FROM gcr.io/deeplearning-platform-release/pytorch-gpu.1-12
-COPY tasks /tasks
-RUN pip install scikit-learn==1.0.2 transformers==4.29.1 datasets==2.12.0 torchvision==0.13.1 accelerate==0.19.0
