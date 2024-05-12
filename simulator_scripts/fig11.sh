0ccab7fc809ce1346bd3bda177249d3149b16d34
Michael Luo <michael.luo@berkeley.edu>
2024-05-12 00:30:24 +0000

    asdf

--- simulator_scripts/fig11.sh
+++ simulator_scripts/fig11.sh
@@ -2,18 +2,16 @@
 
 # Ablating over OO scheduling.
 python run_simulator_sweep.py \
---dataset helios_gen \
---cpus_per_node 48 \
---cluster_size 64 \
---arrival 8 12 16 20 24 25 26 27 28 \
-32 34 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 \
-96 100 104 108 112 116 \
---total_jobs 300000 \
+--dataset philly \
+--cluster_size 16 20 24 28 32 36 40 44 48 \
+52 56 60 64 68 72 76 80 84 88 92 96 100 \
+104 108 112 116 120 124 132 136 140 144 \
 --sched_alg fifo \
---waiting_policy zero-1 constant-0.454 linear_cost_filter_cpu-0.04 linear_capacity_filter_cpu-0.2 \
---loop 0 1 \
+--waiting_policy zero-1 constant-1 linear_cost-0.076 linear_capacity-0.77 \
+--max_queue_length 30 1000000 \
 --long_job_thres -1 0.25 \
+--loop 0 1 \
 --preempt_cloud_ratio -1 3 \
 --seed 1994 \
---filter_name ablate_out_of_order \
---log ~/logs/ablate_out_of_order.log
+--filter_name philly_end2end_ablate_out_of_order \
+--log ~/logs/philly_ablate_out_of_order.log
